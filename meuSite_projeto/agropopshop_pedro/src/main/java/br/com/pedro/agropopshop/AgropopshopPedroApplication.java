02be877e498e81183739ccefec82665f6b2b9563
PedroGurgell <71411486+PedroGurgell@users.noreply.github.com>
2021-04-25 23:16:29 -0300

    Iniciando projeto

---
+++ meuSite_projeto/agropopshop_pedro/src/main/java/br/com/pedro/agropopshop/AgropopshopPedroApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.agropopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgropopshopPedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgropopshopPedroApplication.class, args);
+	}
+
+}
---
+++ meuSite_projeto/agropopshop_pedro/src/test/java/br/com/pedro/agropopshop/AgropopshopPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.agropopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgropopshopPedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
