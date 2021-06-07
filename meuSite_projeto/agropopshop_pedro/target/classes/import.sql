bb8efb93f50f7e2adcad82cb18aabce00910a8b5
PedroGurgell <71411486+PedroGurgell@users.noreply.github.com>
2021-06-07 18:55:21 -0300

    .

---
+++ meuSite_projeto/agropopshop_pedro/target/classes/import.sql
@@ -0,0 +1,6 @@
+﻿/*Primeira linha em branco - Spring não tava adicionado os valores*/
+INSERT INTO clientes (nome, email, endereco, genero, cep, data_atual) VALUES ('Maria da Penha Maia Fernandes', 'mariaDaPenha@gov.br', 'Exemplo de rua', 'Feminino','99999-999', parseDateTime('26/04/2021','dd/MM/yyyy');
+INSERT INTO clientes (nome, email, endereco, genero, cep, data_atual) VALUES ('Pedro Álvares Cabral', 'pedrim@navio.br', 'Exemplo de rua', 'Não Informado','99999-999', parseDateTime('26/04/2021','dd/MM/yyyy'));
+INSERT INTO clientes (nome, email, endereco, genero, cep, data_atual) VALUES ('Tarsila do Amaral', 'tarsila@abaporu.br', 'Exemplo de rua', 'Não Informado','99999-999', parseDateTime('26/04/2021','dd/MM/yyyy'));
+INSERT INTO clientes (nome, email, endereco, genero, cep, data_atual) VALUES ('Oscar Niemeyer', 'obras@niemeyer.br', 'Exemplo de rua', 'Não Informado','99999-999', parseDateTime('26/04/2021','dd/MM/yyyy'));
+INSERT INTO clientes (nome, email, endereco, genero, cep, data_atual) VALUES ('Joaquim Maria Machado de Assis', 'machado@assis.com', 'Exemplo de rua', 'Masculino','99999-999', parseDateTime('26/04/2021','dd/MM/yyyy'));
---
+++ meuSite_projeto/agropopshop_pedro/target/classes/importProduto.sql
@@ -0,0 +1,1 @@
+﻿/*Primeira linha em branco - Spring não tava adicionado os valores*/
